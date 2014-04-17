51cb1db520f7541bac5ffe01c089f7cfc07a680e
jianyaoang <jianyaoang1@gmail.com>
2014-04-16 20:54:24 -0500

    Extracted Reviews JSON and populate review onto TableViewCell. Changed sectionName art to art_Name.

--- Trip'InChicago/PicsAndReviewsViewController.m
+++ Trip'InChicago/PicsAndReviewsViewController.m
@@ -11,6 +11,8 @@
 @interface PicsAndReviewsViewController () <UIScrollViewDelegate, UITableViewDataSource, UITableViewDelegate>
 {
     IBOutlet UITableView *reviewsTableView;
+    NSArray *reviewsText;
+    NSArray *reviewArray;
 }
 
 @end
@@ -29,18 +31,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    reviewArray = [NSArray new];
+    reviewsText = [NSArray new];
+    [self extractReviewJSON];
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return [reviewsText count];
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ReviewsCellID"];
-    cell.textLabel.text = self.mapPoint.referenceKey;
+    cell.textLabel.text = [reviewsText objectAtIndex:indexPath.row];
     return cell;
+}
+
+-(void)extractReviewJSON
+{
+    NSString *urlString = [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/details/json?reference=%@&sensor=true&key=AIzaSyDpJ3qwqVyq08eqvrq5x7_SyPD405ZeWLc",self.mapPoint.referenceKey];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+    {
+        NSDictionary *firstDictionaryLayer = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+        NSDictionary *resultsDictionary = firstDictionaryLayer[@"result"];
+        reviewArray = resultsDictionary[@"reviews"];
+        
+        for (NSDictionary *reviews in reviewArray)
+        {
+            reviewsText = [NSArray arrayWithObject:reviews[@"text"]];
+            NSLog(@"reviewsText:%@",reviewsText);
+        }
+
+        [reviewsTableView reloadData];
+    }];
 }
 
 @end
