ce8e8ab306a510bd203b6b88a0a43abb625b495c
Marion Ano <meaola@gmail.com>
2014-04-19 17:02:47 -0500

    resolve merge conflict

--- Trip'InChicago/MapViewController.m
+++ Trip'InChicago/MapViewController.m
@@ -43,35 +43,11 @@
 
 -(void)extractVenueJSON
 {
-<<<<<<< HEAD
+
     NSString *urlString = [NSString stringWithFormat:@"https://api.foursquare.com/v2/venues/explore?ll=41.8819,-87.6278&near=Chicago&section=%@&oauth_token=02ALL4LOCE2LTXXTA4ASHFTYOEAAUIRWOYT2P5S2AHBBBADA&v=20140419", self.foursquareLocationName];
     NSURL *url = [NSURL URLWithString:urlString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
-=======
 
-   self.sectionMapView.centerCoordinate = userLocation.location.coordinate;
-   self.sectionMapView.region = MKCoordinateRegionMake(userLocation.location.coordinate, MKCoordinateSpanMake(0.03, 0.03));
-
-
-    //[self.sectionMapView setCenterCoordinate:userLocation.coordinate animated:YES];
- //[self.locationManager stopUpdatingLocation];
-    if (!self.didUpdatePins)
-    {
-        self.didUpdatePins = YES;
-        [self queryGooglePlaces:self.googleType];
-    }
-}
-
--(void) queryGooglePlaces: (NSString *) googleType
-{
-    // Build the url string to send to Google. NOTE: The kGOOGLE_API_KEY is a constant that should contain your own API key that you obtain from Google. See this link for more info:
-    // https://developers.google.com/maps/documentation/places/#Authentication
-    NSString *url = [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/place/search/json?location=%f,%f&radius=%d&types=%@&sensor=true&maxprice=2&rankby=prominence&key=%@", self.sectionMapView.userLocation.coordinate.latitude, self.sectionMapView.userLocation.coordinate.longitude, 500, googleType, kGOOGLE_API_KEY]; //took out self.currentCenter.latitude & , self.currentCenter.longitude
-
-    //NSLog(@" this is %f %f", self.currentCenter.latitude, self.currentCenter.longitude);
-    //NSLog(@"%@", url);
->>>>>>> f0aa9849ebd2c07e07441b9b0bfc29f5e2ad8639
-    
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
         NSDictionary *firstLayer = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
         NSDictionary *responseDictionary = firstLayer[@"response"];
