df7ce458a61b086bfb3614152d39f180d775d985
jaime-h <jaime.e.hernandez@gmail.com>
2014-04-21 18:30:31 -0500

    Adding Concierge UIView Map

---
+++ Trip'InChicago/RouteMapViewController.m
@@ -0,0 +1,33 @@
+//
+//  RouteMapViewController.m
+//  Trip'InChicago
+//
+//  Created by Jaime Hernandez on 4/21/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import "RouteMapViewController.h"
+
+@interface RouteMapViewController ()
+
+@end
+
+@implementation RouteMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+
+@end
