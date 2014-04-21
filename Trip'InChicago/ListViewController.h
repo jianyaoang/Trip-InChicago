30907edf9fb1d82cb1c93e1aea39ccbd0f046b47
Marion Ano <meaola@gmail.com>
2014-04-21 12:50:39 -0500

    Added Concierge ViewController and ListViewController

---
+++ Trip'InChicago/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  Trip'InChicago
+//
+//  Created by Marion Ano on 4/21/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UIViewController
+
+@end
---
+++ Trip'InChicago/ListViewController.m
@@ -0,0 +1,65 @@
+//
+//  ListViewController.m
+//  Trip'InChicago
+//
+//  Created by Marion Ano on 4/21/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import "ListViewController.h"
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import "Location.h"
+
+@interface ListViewController () <UITableViewDataSource, UITableViewDataSource, CLLocationManagerDelegate>
+
+
+@end
+
+@implementation ListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -- table view delegate methods
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ReuseCellID"];
+    return cell;
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
