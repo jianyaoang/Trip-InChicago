b12898e7054d3fa123018e1b94be497baf19b5d3
Marion Ano <meaola@gmail.com>
2014-04-28 09:21:59 -0500

    Added in SectionListViewController custom class

---
+++ Trip'InChicago/SectionListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SectionListViewController.h
+//  Trip'InChicago
+//
+//  Created by Marion Ano on 4/28/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SectionListViewController : UIViewController
+
+@end
